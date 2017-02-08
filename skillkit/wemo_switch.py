cb47635db8f3468007b49e4a059e6108682e8b7d
vincent chung <vincentcw.chung@gmail.com>
2017-02-08 22:49:36 +0800

    adding REST api for control wemo switch

--- skillkit/wemo_switch.py
+++ skillkit/wemo_switch.py
@@ -3,6 +3,22 @@ from ouimeaux.environment import Environment
 
 switch_cmd=''
 
+def switch_on():
+    global switch_cmd
+    switch_cmd='on'
+    env = Environment(on_switch, on_motion)
+    env.start()
+    env.discover(seconds=3)
+    return "sending command.."
+
+def switch_off():
+    global switch_cmd
+    switch_cmd='off'
+    env = Environment(on_switch, on_motion)
+    env.start()
+    env.discover(seconds=3)
+    return "sending command.."
+
 def on_switch(switch):
      global switch_cmd
      print('********  Switch found:'+ switch.name+ ', Current state : '+str(switch.get_state()))
@@ -29,12 +45,14 @@ def main():
 
     if(switch_cmd=='on'):
         print "turning on..."
+        switch_on()
     else:
         print "turning off..."
+        switch_off()
 
-    env = Environment(on_switch, on_motion)
-    env.start()
-    env.discover(seconds=3)
+    #env = Environment(on_switch, on_motion)
+    #env.start()
+    #env.discover(seconds=3)
 
 #main function
 if __name__ == "__main__":
