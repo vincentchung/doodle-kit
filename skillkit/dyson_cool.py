edd82eb354cc575c4a0822ffa07adcf3555874f6
vincent chung <vincentcw.chung@gmail.com>
2017-10-05 15:05:10 +0800

    adding remind feature to control dysoncool

--- skillkit/dyson_cool.py
+++ skillkit/dyson_cool.py
@@ -4,9 +4,25 @@ from broadlinkRM import *
 
 dysonCoolPowerSwitch="skillkit/dysoncool/powerSwitch"
 dysonCoolSpinSwitch="skillkit/dysoncool/spinSwitch"
+dysonCoolFanDec="skillkit/dysoncool/windDown"
+dysonCoolFanInc="skillkit/dysoncool/windUp"
+dysonCoolTimerInc="skillkit/dysoncool/timer_inc"
+dysonCoolTimerDec="skillkit/dysoncool/timer_dec"
 
 def powerOn():
     commandConnectExecute(dysonCoolPowerSwitch)
 
 def spinOn():
     commandConnectExecute(dysonCoolSpinSwitch)
+
+def fanAirDec():
+    commandConnectExecute(dysonCoolFanDec)
+
+def fanAirInc():
+    commandConnectExecute(dysonCoolFanInc)
+
+def timerAdjInc():
+    commandConnectExecute(dysonCoolTimerInc)
+
+def timerAdjDec():
+    commandConnectExecute(dysonCoolTimerDec)
