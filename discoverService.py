f3dd1058a72f8e043c9cab9f52d40e89d99ba257
Vincent <vincentcw.chung@gmail.com>
2018-04-19 16:21:38 +0800

    fixing python space issue in windows

--- discoverService.py
+++ discoverService.py
@@ -6,7 +6,7 @@
 # Usage:
 #   mcast -s (sender, IPv4)
 #   mcast -s -6 (sender, IPv6)
-#   mcast    (receivers, IPv4)
+#   mcast  (receivers, IPv4)
 #   mcast  -6  (receivers, IPv6)
 
 MYPORT = 8123
@@ -20,58 +20,58 @@ import socket
 import sys
 
 def main():
-    group = MYGROUP_6 if "-6" in sys.argv[1:] else MYGROUP_4
+  group = MYGROUP_6 if "-6" in sys.argv[1:] else MYGROUP_4
 
-    receiver(group)
+   receiver(group)
 
 
 def sender(group):
-    addrinfo = socket.getaddrinfo(group, None)[0]
+   addrinfo = socket.getaddrinfo(group, None)[0]
 
-    s = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
+   s = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
 
-    # Set Time-to-live (optional)
-    ttl_bin = struct.pack('@i', MYTTL)
-    if addrinfo[0] == socket.AF_INET: # IPv4
-        s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl_bin)
-    else:
-        s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
+   # Set Time-to-live (optional)
+   ttl_bin = struct.pack('@i', MYTTL)
+   if addrinfo[0] == socket.AF_INET: # IPv4
+      s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl_bin)
+   else:
+      s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
 
-    while True:
-        data = repr(time.time())
-        s.sendto(data + '\0', (addrinfo[4][0], MYPORT))
-        time.sleep(1)
+   while True:
+      data = repr(time.time())
+      s.sendto(data + '\0', (addrinfo[4][0], MYPORT))
+      time.sleep(1)
 
 
 def receiver(group):
-    # Look up multicast group address in name server and find out IP version
-    addrinfo = socket.getaddrinfo(group, None)[0]
+   # Look up multicast group address in name server and find out IP version
+   addrinfo = socket.getaddrinfo(group, None)[0]
 
-    # Create a socket
-    s = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
+   # Create a socket
+   s = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
 
-    # Allow multiple copies of this program on one machine
-    # (not strictly needed)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+   # Allow multiple copies of this program on one machine
+   # (not strictly needed)
+   s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    # Bind it to the port
-    s.bind(('', MYPORT))
+   # Bind it to the port
+   s.bind(('', MYPORT))
 
-    group_bin = socket.inet_pton(addrinfo[0], addrinfo[4][0])
-    # Join group
-    if addrinfo[0] == socket.AF_INET: # IPv4
-        mreq = group_bin + struct.pack('=I', socket.INADDR_ANY)
-        s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-    else:
-        mreq = group_bin + struct.pack('@I', 0)
-        s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+   group_bin = socket.inet_pton(addrinfo[0], addrinfo[4][0])
+   # Join group
+   if addrinfo[0] == socket.AF_INET: # IPv4
+      mreq = group_bin + struct.pack('=I', socket.INADDR_ANY)
+      s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+   else:
+      mreq = group_bin + struct.pack('@I', 0)
+      s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
 
-    # Loop, printing any data we receive
-    while True:
-        data, sender = s.recvfrom(1500)
-        while data[-1:] == '\0': data = data[:-1] # Strip trailing \0's
-        print (str(sender) + '  ' + repr(data))
+   # Loop, printing any data we receive
+   while True:
+      data, sender = s.recvfrom(1500)
+      while data[-1:] == '\0': data = data[:-1] # Strip trailing \0's
+      print (str(sender) + '  ' + repr(data))
 
 
 if __name__ == '__main__':
-    main()
+   main()
--- webserver.py
+++ webserver.py
@@ -13,56 +13,57 @@ import skillkit
 #each block image of vitrulButton
 
 urls = (
-    '/skillkit', 'ListSkillkit',
-    '/vitrulButton', 'ListSkillkit',
-    '/skillkit/(.*)', 'LaunchSkillkit',
-    '/images/(.*)', 'images' #this is where the image folder is located....
+   '/skillkit', 'ListSkillkit',
+   '/vitrulButton', 'ListSkillkit',
+   '/skillkit/(.*)', 'LaunchSkillkit',
+   '/images/(.*)', 'images' #this is where the image folder is located....
 )
 
 skillkit_dat='skillkit.json'
 
 def DBread():
-    # Reading data back
-    data=''
-    with open(skillkit_dat, 'r') as f:
-        data = json.load(f)
-    return data
+   # Reading data back
+   data=''
+   with open(skillkit_dat, 'r') as f:
+     data = json.load(f)
+
+   return data
 
 def DBupdate(obj):
-    # Reading data back
-    with open('data.json', 'w') as f:
-        json.dump(obj, f)
+   # Reading data back
+   with open('data.json', 'w') as f:
+     json.dump(obj, f)
 
 app = web.application(urls, globals())
 class images:
-    def GET(self,name):
-        ext = name.split(".")[-1] # Gather extension
+   def GET(self,name):
+     ext = name.split(".")[-1] # Gather extension
 
-        cType = {
-            "png":"images/png",
-            "jpg":"images/jpeg",
-            "gif":"images/gif",
-            "ico":"images/x-icon"            }
+     cType = {
+     "png":"images/png",
+     "jpg":"images/jpeg",
+     "gif":"images/gif",
+     "ico":"images/x-icon"   }
 
-        if name in os.listdir('images'):  # Security
-            web.header("Content-Type", cType[ext]) # Set the Header
-            return open('images/%s'%name,"rb").read() # Notice 'rb' for reading images
-        else:
-            raise web.notfound()
+     if name in os.listdir('images'):  # Security
+       web.header("Content-Type", cType[ext]) # Set the Header
+       return open('images/%s'%name,"rb").read() # Notice 'rb' for reading images
+     else:
+       raise web.notfound()
 
 class ListSkillkit:
-    def GET(self):
-	output = skillkit.getSkillkitlist()
-        return output
+   def GET(self):
+     output = skillkit.getSkillkitlist()
+     return output
 
 class LaunchSkillkit:
-    def GET(self, user):
-        #user is the * part string...
-        #trigger skillkit working
-        output=skillkit.launchSkill(user)
+   def GET(self, user):
+     #user is the * part string...
+     #trigger skillkit working
+     output=skillkit.launchSkill(user)
 
-        #output=DBread()
-        return output
+     #output=DBread()
+     return output
 
 if __name__ == "__main__":
-    app.run()
+   app.run()
