446f95129dc7990dbf82c6f3c8f1e7dbab4793be
Vincent <vincentcw.chung@gmail.com>
2018-04-23 13:59:55 +0800

    fixing merge git

--- skillkit/SamsungS8.py
+++ skillkit/SamsungS8.py
@@ -1,8 +1,16 @@
 import mongoDB
+import time
+import datetime
 
 def sensorupdate(d):
     print(d)
-    mongoDB.adddata({"name":"zhangsan","age":18})
+    a=str(d).split(",")
+    timestamp = time.mktime(datetime.datetime.now())
+    #adding timestamp
+    data={"name":a[0],"data1":a[1]}
+
+
+    mongoDB.adddata({"name":a[0],"data"+str(i):a[1i]})
     return "2"
 
 def sensorupdate1():
