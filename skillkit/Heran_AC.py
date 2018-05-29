8abc05f01943868a8bdfa4936a80ac7226db64e3
Vincent <vincentcw.chung@gmail.com>
2018-05-29 16:59:10 +0800

    add new cp application

--- skillkit/Heran_AC.py
+++ skillkit/Heran_AC.py
@@ -2,24 +2,51 @@ import sys
 sys.path.insert(0, './skillkit')
 from broadlinkRM import *
 
-heranACPowerSwitch="skillkit/AC_Heran/AC_power_on"
-heranACSpinSwitch="skillkit/AC_Heran/spinSwitch"
-heranACFanDec="skillkit/AC_Heran/windDown"
-heranACFanInc="skillkit/AC_Heran/windUp"
-heranACTP24="skillkit/AC_Heran/AC_temperture_24"
-heranACTP25="skillkit/AC_Heran/AC_temperture_25"
+heranACPowerSwitchON="skillkit/AC_Heran/AC_power_on"
+heranACPowerSwitchOFF="skillkit/AC_Heran/AC_power_off"
+heranACFan="skillkit/AC_Heran/AC_fan_change"
+heranACTP="skillkit/AC_Heran/AC_temperture_"
+heranACSleepMode="skillkit/AC_Heran/AC_feature_sleep"
+
+fanLevel = 1
+temperature = 25
+powerButton = 0
 
 def powerOn():
-    commandConnectExecute(heranACPowerSwitch)
+  if(powerButton==0):
+    powerButton=1
+    commandConnectExecute(heranACPowerSwitchON)
+  else:
+    powerButton=0
+    commandConnectExecute(heranACPowerSwitchOFF)
+
 
 def temperatureInc():
-    commandConnectExecute(heranACTP24)
+    temperature+=1
+    str=heranACTP+str(temperature)
+    commandConnectExecute(str)
 
 def temperatureDec():
-    commandConnectExecute(heranACTP25)
+    temperature-=1
+    str=heranACTP+str(temperature)
+    commandConnectExecute(str)
 
 def fanAirInc():
-    commandConnectExecute(heranACFanInc)
+    fanLevel-=1
+    if(fanLevel==0):
+      fanLevel=1
+    str=heranACFan+str(fanLevel)
+    commandConnectExecute(str)
+
+def fanAirDec():
+    fanLevel+=1
+    if(fanLevel>3):
+      fanLevel=3
+    str=heranACFan+str(fanLevel)
+    commandConnectExecute(str)
+
+def sleepMode():
+    commandConnectExecute(heranACSleepMode)
 
 def timerAdjInc():
     commandConnectExecute(heranACTimerInc)
---
+++ visionkit/Objtracker.py
@@ -0,0 +1,142 @@
+# USAGE
+# python track.py --video video/iphonecase.mov
+
+# import the necessary packages
+import numpy as np
+#from pyimagesearch.facedetector import FaceDetector
+import argparse
+import time
+import cv2
+
+#config
+minarea=80
+firstFrame = None
+objcount=0
+faceCurrentNum=0
+faceRects=None
+
+class FaceDetector:
+	def __init__(self, faceCascadePath):
+		# load the face detector
+		self.faceCascade = cv2.CascadeClassifier(faceCascadePath)
+
+	def detect(self, image, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30)):
+		# detect faces in the image
+		rects = self.faceCascade.detectMultiScale(image,
+			scaleFactor = scaleFactor, minNeighbors = minNeighbors,
+			minSize = minSize, flags = cv2.CASCADE_SCALE_IMAGE)
+
+		# return the rectangles representing bounding
+		# boxes around the faces
+		return rects
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--face", required = True,
+	help = "path to where the face cascade resides")
+
+ap.add_argument("-v", "--video",
+	help = "path to the (optional) video file")
+args = vars(ap.parse_args())
+
+fd = FaceDetector(args["face"])
+
+if not args.get("video", False):
+	camera = cv2.VideoCapture(0)
+else:
+    camera = cv2.VideoCapture(args["video"])
+
+# keep looping
+while True:
+	# grab the current frame
+	(grabbed, frame) = camera.read()
+
+	# check to see if we have reached the end of the
+	# video
+	if not grabbed:
+		break
+    #tracker implment
+	#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	#dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+	#ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+	#x,y,w,h = track_window
+	#cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
+	# determine which pixels fall within the blue boundaries
+	# and then blur the binary image
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (3, 3), 0)
+
+	if firstFrame is None:
+		firstFrame = gray
+		continue
+
+	frameDelta = cv2.absdiff(firstFrame, gray)
+	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+	thresh = cv2.dilate(thresh, None, iterations=2)
+	(_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+
+	largestarea=0
+	x=0
+	y=0
+	w=0
+	h=0
+
+	area_counter=0;
+	for c in cnts:
+		#print(cv2.contourArea(c))
+		if(cv2.contourArea(c)>minarea):
+			area_counter+=1
+			#print(cv2.contourArea(c))
+			(x,y,w,h) = cv2.boundingRect(c)
+			#cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+		if(cv2.contourArea(c)>largestarea):
+			(x,y,w,h) = cv2.boundingRect(c)
+
+	#cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+
+
+	#cv2.putText(frame, (str(cv2.contourArea(c))), (x, y),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+	if objcount != area_counter:
+		print("trigger!!:"+str(objcount))
+		firstFrame=gray
+		objcount=area_counter
+        # find faces in the image
+			#faceframe=frame[y:y + h,x:x + w]
+		faceframe=gray
+		faceRects = fd.detect(faceframe, scaleFactor = 1.1, minNeighbors = 5,
+		minSize = (30, 30))
+
+		if(len(faceRects)>0):
+			print("I found {} face(s)".format(len(faceRects)))
+			faceCurrentNum=len(faceRects)
+		else:
+			faceCurrentNum=0
+		# loop over the faces and draw a rectangle around each
+		#for (fx, fy, w, h) in faceRects:
+		#	cv2.rectangle(frame, (x+fx, y+fy), (x+fx + w, y+fy + h), (255, 0, 0), 1)
+
+	if(faceCurrentNum>0):
+		for (fx, fy, w, h) in faceRects:
+			cv2.rectangle(frame, (fx, fy), (fx + w, fy + h), (255, 0, 0), 1)
+
+	cv2.putText(frame, ("face:"+str(faceCurrentNum)), (0, 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+# show the frame and the binary image
+	cv2.imshow("Tracking", frame)
+	cv2.imshow("Binary", thresh)
+
+	# if your machine is fast, it may display the frames in
+	# what appears to be 'fast forward' since more than 32
+	# frames per second are being displayed -- a simple hack
+	# is just to sleep for a tiny bit in between frames;
+	# however, if your computer is slow, you probably want to
+	# comment out this line
+	time.sleep(0.025)
+
+	# if the 'q' key is pressed, stop the loop
+	if cv2.waitKey(1) & 0xFF == ord("q"):
+		break
+
+# cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
